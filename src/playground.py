65142a82be62d84ff9cd9883582575580528b862
Amin Rnx <amin.raji96@hotmail.com>
2025-05-26 09:13:49 +0330

    SolvedImageLoadingIssue(FixPoint)

--- src/playground.py
+++ src/playground.py
@@ -2,9 +2,13 @@ from torch.utils.data import DataLoader
 from dataset import ChestXRay14
 from torch.multiprocessing import freeze_support
 
+#import os
+#print("UNDER data/:", os.listdir("data/"))
+#print("UNDER data/images:", os.listdir("data/images"))
+
 if __name__ == "__main__":
     freeze_support()  # For Windows compatibility
-    ds = ChestXRay14(img_dir='data/images_001/images', csv_file='cxr_csv/Data_Entry_2017.csv')
+    ds = ChestXRay14(img_dir='data/images', csv_file='cxr_csv/Data_Entry_2017.csv')
     dl = DataLoader(ds, batch_size=16, shuffle=True, num_workers=4)
 
     x, y = next(iter(dl))
